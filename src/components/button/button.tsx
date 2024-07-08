7f6c7a4877f59b1f70e8ecc399ee06d42d0b81f4
Robocotik <alexander.starkin@gmail.com>
2024-07-08 17:19:25 +0300

    [REWRITE] button.tsx

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,27 +1,20 @@
 import React from "react";
+import { useState } from "react";
+
 import "./button.css";
 
 
 const Button = ({ text = "empty", isFill = true }: { text: string, isFill: boolean }) => {
-    const fillFlag = isFill ? 'isFill' : 'isTransparent';
+    // const fillFlag = isFill ? 'isFill' : 'isTransparent';
+    const [fillState, setFill] = useState(isFill)
 
-    const switchFill = (e) => {
-        e.preventDefault();
-        let obj = e.target;
-        console.log('Была нажата ссылка.');
-        if (obj?.classList.contains("isFill")) {
-            obj.classList.remove("isFill");
-            obj.classList.add("isTransparent");
-        }
-        else {
-            obj?.classList.remove("isTransparent");
-            obj?.classList.add("isFill");
-        }
+    const switchFill = () => {
+        setFill(!fillState);
     }
 
     return (
         <div>
-            <button className={`App__button App_textStyleBase ${fillFlag}`} onClick={switchFill}>{text}</button>
+            <button className={`App__button ${fillState? 'App__button_fulled' : ''}`} onClick={switchFill}>{text}</button>
         </div >
     );
 
